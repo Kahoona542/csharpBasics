f89c84de132aba4a4d68b9a03fa45e480418a8e0
Robin Hancock <hancockrobin543@gmail.com>
2022-02-02 16:11:28 -0500

    added

--- 02_Types/Program.cs
+++ 02_Types/Program.cs
@@ -1,15 +1,24 @@
-﻿bool isLoggedIn = true;
+﻿//bool is a true false value, no quotes around the word
+//this is because true and false are keywords, not strings
+bool isLoggedIn = true;
 Console.WriteLine(isLoggedIn);
 
+//integers, or int, are whole numbers only
 int age = 42;
 Console.WriteLine(age);
 
+//Double is like decimal but less precise. 
 double balanceDue = 800.99d;
 Console.WriteLine(balanceDue);
+//Notice that when the accountBalance gets written to the console there is no m suffix in the optput
 
+//decimal has a lot of possible decimal points
+//it also needs the m suffix to know its a decimal type
 decimal accountBalance = 9000.01m;
 Console.WriteLine(accountBalance);
+//Notice that when the accountBalance gets written to the console there is no m suffix in the optput
 
+//Floats can't be a whole number
 float averageHeight = 5.7f;
 Console.WriteLine(averageHeight);
 
