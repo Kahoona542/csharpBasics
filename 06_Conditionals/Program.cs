243ad6ea80a7a0929ea16605b2f7e8457f5549d5
Robin Hancock <hancockrobin543@gmail.com>
2022-02-03 10:20:07 -0500

    conditionals added

---
+++ 06_Conditionals/Program.cs
@@ -0,0 +1,46 @@
+﻿bool isSnowing = true;
+bool isGoingOutside = false;
+//if(true/false value)
+//{
+//  body of the code, execute this code if above is true
+//}
+if(isSnowing)
+{
+    Console.WriteLine("It is snowing out!");
+}
+
+if(isSnowing && isGoingOutside)
+{
+    Console.WriteLine("Better where some snow boots!!!");
+}
+if(!isSnowing || !isGoingOutside)
+{
+    Console.WriteLine("I do not need any boots!!");
+}
+if(isSnowing ^ isGoingOutside)
+{
+    Console.WriteLine("I will only go outside once the snow stops!!!");
+}
+
+
+if(!isSnowing)
+{
+    Console.WriteLine("There is no need to shovel snow!");
+}
+
+else{
+    Console.WriteLine("I guess i need to shovel the snow");
+}
+
+if(isSnowing)
+{
+    Console.WriteLine("I guess i have to go clear off the wife's car.");
+}
+else if(!isSnowing)
+{
+    Console.WriteLine("I don't have to clear off the cars!");
+}
+else{
+    Console.WriteLine("I don't know what we are talking about");
+    )
+}
